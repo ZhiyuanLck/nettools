c95df9fe8896f22b8c4d68daefb71bf0c67bd2ed
ZhiyuanLck <lichangkai225@qq.com>
2021-08-23 16:35:57 +0800

    fix bug

--- src/ping.cpp
+++ src/ping.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
 #include <string>
+#include <boost/system.hpp>
 #include <boost/asio.hpp>
 #include <boost/bind.hpp>
-#include <boost/asio/steady_timer.hpp>
 #include <algorithm>
-// #include <fmt/core.h>
-// #include <chrono>
 
 #include "header.hpp"
 
 namespace asio = boost::asio;
+using boost::system::error_code;
 
 namespace nettool {
 
@@ -17,30 +16,53 @@ namespace posix_time = boost::posix_time;
 using asio::ip::icmp;
 using asio::deadline_timer;
 
+
+// 1. Resolve destination address with DNS resolver
+// 2. Construct and send ICMP message
+//    -> wait for timeout or signal for a valid return message
+//    -> sent the next message
+// 3. Prepare buffer -> handle received messages -> receive next
 class pinger {
 public:
   pinger(asio::io_service& io_service, const char* destination)
       : resolver_(io_service), socket_(io_service, icmp::v4()),
       timer_(io_service), sequence_number_(0), num_replies_(0) {
-    // protocol, services(port num), resolve_flags
+    // basic_resolver: protocol, services, flags
     icmp::resolver::query query(icmp::v4(), destination, "");
-    // resolve查询返回一个迭代器，代表查询到的ip端点
+    // a iterator of queried endpoint is returned
     destination_ = *resolver_.resolve(query);
 
     start_send();
     start_receive();
   }
 private:
+  // Consider the time to send the message
+  // 1. First send, every thing is ok
+  // 2. Send after 5s's timeout call, and if the valid return message is
+  // arrived almost simultaneously or later.
+  //   1) If the sequence number has increased, so the message is
+  //   invalid
+  //   2) If the sequence number has not increased, we need to check
+  //   whether the received message is timeout:
+  //     i) If the send time is not reset, the message must be timeout,
+  //     then is invalid.
+  //     ii) If the send time is reset, the message may be deemed to have
+  //     arrived in time, but the sequence number has increased, so the message
+  //     is invalid too.
+  // 3. Send after a valid return message: since we have not sent the next
+  // message, there should be no valid return message to be received
   void start_send() {
-    std::string body("Some message.");
+    std::string body(56, 'z');
 
-    // Init header
+    // Construct header
+    // The protocol of ip::icmp is IPPROTO_ICMP, so the kernel will
+    // automatically add the correct ip header
     icmp_header echo_request;
     echo_request.type(icmp_header::echo_request);
-    echo_request.code(0); // 回显应答
+    echo_request.code(0); // for echo request/reply
     echo_request.identifier(get_identifier());
     echo_request.sequence_number(++sequence_number_);
-    compute_checksum(echo_request, body.begin(), body.end()); // 计算校验和
+    compute_checksum(echo_request, body.begin(), body.end());
 
     // Encode
     asio::streambuf request_buffer;
@@ -51,71 +73,75 @@ private:
     time_sent_ = posix_time::microsec_clock::universal_time();
     socket_.send_to(request_buffer.data(), destination_);
 
-    // 等待回复5s
+    // Set a timer of 5s, whose handle may be called when a valid message is
+    // detected.
     num_replies_ = 0;
     timer_.expires_at(time_sent_ + posix_time::seconds(5));
-    timer_.async_wait(boost::bind(&pinger::handle_timeout, this));
+    timer_.async_wait(boost::bind(&pinger::handle_timeout, this, asio::placeholders::error));
   }
 
-  void handle_timeout() {
-    if (num_replies_ == 0)
+  void handle_timeout(const error_code& ec) {
+    if (num_replies_ == 0 && !ec)
       std::cout << "Request timed out" << std::endl;
+    if (ec && ec.value() != boost::system::errc::operation_canceled)
+      std::cerr << ec.message() << std::endl;
 
-    // 1s后再发送
+    // Send the next request after at least 1s
     timer_.expires_at(time_sent_ + posix_time::seconds(1));
     timer_.async_wait(boost::bind(&pinger::start_send, this));
   }
 
   void start_receive() {
-    // 丢弃已有的数据
+    // Discard possible data
     reply_buffer_.consume(reply_buffer_.size());
-    // 准备接收最多64KB的数据
+    // Prepare the buffer for at most 64KB data
     socket_.async_receive(reply_buffer_.prepare(65536),
-        boost::bind(&pinger::handle_receive, this, asio::placeholders::bytes_transferred));
+        boost::bind(&pinger::handle_receive, this, asio::placeholders::error, asio::placeholders::bytes_transferred));
   }
 
-  void handle_receive(std::size_t length) {
-    reply_buffer_.commit(length);
+  void handle_receive(const error_code& ec, std::size_t length) {
+    if (ec)
+      std::cerr << ec.message() << std::endl;
+    else {
+      reply_buffer_.commit(length);
 
-    // Decode
-    std::istream is(&reply_buffer_);
-    ipv4_header ipv4_hdr;
-    icmp_header icmp_hdr;
-    is >> ipv4_hdr >> icmp_hdr;
+      // Discard timeout return message
+      posix_time::ptime tmp = posix_time::microsec_clock::universal_time();
+      if ((tmp - time_sent_).total_milliseconds() > 5000) return;
 
-    // 过滤，只需要回显消息以及表示符匹配的消息
-    if (is && icmp_hdr.type() == icmp_header::echo_reply
-        && icmp_hdr.identifier() == get_identifier()
-        && icmp_hdr.sequence_number() == sequence_number_) {
-      // 收到第一个回复时中断5s的定时器
-      if (num_replies_++ == 0)
-        timer_.cancel();
-      posix_time::ptime now = posix_time::microsec_clock::universal_time();
-      std::cout << length - ipv4_hdr.header_length()
-        << " bytes from " << ipv4_hdr.source_address()
-        << ": icmp_seq=" << icmp_hdr.sequence_number()
-        << ", ttl=" << ipv4_hdr.time_to_live()
-        << ", time=" << (now - time_sent_).total_milliseconds() << " ms"
-        << std::endl;
-      // fmt::print("{0} bytes from {1}: icmp_seq = {2}, ttl = {3}, time = {4} ms",
-          // length - ipv4_hdr.header_length(),
-          // ipv4_hdr.source_address(),
-          // icmp_hdr.sequence_number(),
-          // ipv4_hdr.time_to_live(),
-          // (now - time_sent_).total_milliseconds());
+      // Decode
+      std::istream is(&reply_buffer_);
+      ipv4_header ipv4_hdr;
+      icmp_header icmp_hdr;
+      is >> ipv4_hdr >> icmp_hdr;
 
-      start_receive();
+      // Filter the message we are interested
+      if (is && icmp_hdr.type() == icmp_header::echo_reply
+          && icmp_hdr.identifier() == get_identifier()
+          && icmp_hdr.sequence_number() == sequence_number_) {
+        // Call handle_timeout only when the first valid message arrive
+        if (num_replies_++ == 0)
+          timer_.cancel();
+        posix_time::ptime now = posix_time::microsec_clock::universal_time();
+        std::cout << length - ipv4_hdr.header_length()
+          << " bytes from " << ipv4_hdr.source_address()
+          << ": icmp_seq=" << icmp_hdr.sequence_number()
+          << ", ttl=" << ipv4_hdr.time_to_live()
+          << ", time=" << (now - time_sent_).total_microseconds() / 1000.0 << " ms"
+          << std::endl;
+      }
     }
+    start_receive();
   }
 
   static unsigned short get_identifier() {
     return static_cast<unsigned short>(::getpid());
   }
 
-  icmp::resolver resolver_; // DNS解析
-  icmp::endpoint destination_; // 目标套接字端点
-  icmp::socket socket_; // stream_socket
-  deadline_timer timer_; // 定时器
+  icmp::resolver resolver_;
+  icmp::endpoint destination_;
+  icmp::socket socket_; // raw socket
+  deadline_timer timer_;
   unsigned short sequence_number_;
   posix_time::ptime time_sent_;
   asio::streambuf reply_buffer_;
@@ -131,9 +157,10 @@ int main(int argc, char* argv[]) {
       return 1;
     }
 
+    error_code ec;
     asio::io_service io_service;
     nettool::pinger p(io_service, argv[1]);
-    io_service.run();
+    io_service.run(ec);
   } catch (std::exception& e) {
     std::cerr << "Exception: " << e.what() << std::endl;
   }
